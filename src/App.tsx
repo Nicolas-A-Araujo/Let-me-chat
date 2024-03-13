1f3bec30d2c3d79b230af103a8cc8b0f2a0dba3f
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-03-13 18:24:16 -0300

    Add new routes and component AuthContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NewRoom } from "./pages/NewRoom";
+import { Home } from "./pages/Home";
+import { AuthContextProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/*" Component={Home}/>
+          <Route path="rooms/new" Component={NewRoom}/>
+        </Routes>
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
