bf6a6cce566e2883e3cfa4d132a8a6a65c5dbdba
Nícolas Alencar de Araújo <titanalenujo@gmail.com>
2024-03-16 00:47:22 -0300

    Admin route (/admin/room/:id)

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Home } from "./pages/Home";
 import { Room } from "./pages/Room";
 
 import { AuthContextProvider } from "./contexts/AuthContext";
+import { AdminRoom } from "./pages/AdminRoom";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
           <Route path="/*" Component={Home}/>
           <Route path="/rooms/new" Component={NewRoom}/>
           <Route path="/rooms/:id" Component={Room}/>
+
+          <Route path="/admin/rooms/:id" Component={AdminRoom}/>
         </Routes>
       </AuthContextProvider>
     </BrowserRouter>
